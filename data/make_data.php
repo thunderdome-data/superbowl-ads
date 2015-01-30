3e98c59eea386ece2ce3af76bb4eeb8269f8f171
freejoe76 <jmurphy@denverpost.com>
2015-01-30 13:31:20 -0700

    Start on the 2015 data work

--- data/make_data.php
+++ data/make_data.php
@@ -1,10 +1,14 @@
 <?php
 $icons = array('6' => 'icon-animals.png','7' => 'icon-celeb.png','8' => 'icon-children.png', '9' => 'icon-humor.png', '10' => 'icon-sex.png');
-$f = 'data_2014.txt';
+$f = 'data_2015.txt';
 $lines = file($f);
 //array_shift($lines);
 foreach($lines as $line) {
     $pieces = explode("\t",$line);
+
+    // Remove the timestamp field, which is in the spreadsheet because we used Google Forms to input the data.
+    array_shift($pieces);
+
     $pieces[5] = make_link($pieces[5]);
     $cats = '';
     for($i=6; $i<=10;$i++) {
@@ -17,10 +21,18 @@ foreach($lines as $line) {
     echo implode("\t",$pieces) . "\n";
 
 }
-function make_link($url) {
-    $pieces = explode('/',trim($url));
-    $id = array_pop($pieces);
-    $id = preg_replace('%watch\?v=%','',$id);
-    return '<button type="button"  class="video-button" onclick="show_video(' . "'" . $id . "'" . ')">Watch ad</button>';
-
+function make_link($url) 
+{
+    if ( $strpos($url, 'youtube') > -1 ):
+        $pieces = explode('/',trim($url));
+        $id = array_pop($pieces);
+        $id = preg_replace('%watch\?v=%','',$id);
+        $video_type = 'youtube';
+    elseif ( strpos($url, 'bcove') > -1 0:
+        $id = array_pop(explode('/', $url));
+        $video_type = 'bcove';
+    else:
+        return '';
+    endif;
+    return '<button type="button"  class="video-button" onclick="show_video(\'' . $id . '\', \'' . $video_type . '\')">Watch ad</button>';
 }
