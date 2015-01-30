931f2b26d2c3cd75ebb7f361b91d17b44926bbb1
freejoe76 <jmurphy@denverpost.com>
2015-01-30 13:52:30 -0700

    Data wrangling.

--- data/make_data.php
+++ data/make_data.php
@@ -1,13 +1,35 @@
 <?php
 $icons = array('6' => 'icon-animals.png','7' => 'icon-celeb.png','8' => 'icon-children.png', '9' => 'icon-humor.png', '10' => 'icon-sex.png');
-$f = 'data_2015.txt';
+$f = 'data_2015.tsv';
 $lines = file($f);
-//array_shift($lines);
 foreach($lines as $line) {
+echo $line;
     $pieces = explode("\t",$line);
 
     // Remove the timestamp field, which is in the spreadsheet because we used Google Forms to input the data.
     array_shift($pieces);
+
+    // Other shifting to account for order of columns in the Google Form:
+    $desc = $pieces[2];
+    $title = $pieces[3];
+    $pieces[2] = $title;
+    $pieces[3] = $desc;
+
+/*
+// The categories as they exist in the app.
+0        data.addColumn("number","Year");
+1        data.addColumn("string","Advertiser");
+2        data.addColumn("string","Title");
+3        data.addColumn("string","Description");
+4        data.addColumn("string","Category");
+5        data.addColumn("string"," "); <-- This is where the video embed stuff goes.
+6        data.addColumn("string","Animals");
+7        data.addColumn("string","Celebrity");
+8        data.addColumn("string","Children");
+9        data.addColumn("string","Humor");
+10        data.addColumn("string","Sex");
+
+*/
 
     $pieces[5] = make_link($pieces[5]);
     $cats = '';
@@ -23,12 +45,12 @@ foreach($lines as $line) {
 }
 function make_link($url) 
 {
-    if ( $strpos($url, 'youtube') > -1 ):
+    if ( strpos($url, 'youtube') > -1 ):
         $pieces = explode('/',trim($url));
         $id = array_pop($pieces);
         $id = preg_replace('%watch\?v=%','',$id);
         $video_type = 'youtube';
-    elseif ( strpos($url, 'bcove') > -1 0:
+    elseif ( strpos($url, 'bcove') > -1 ):
         $id = array_pop(explode('/', $url));
         $video_type = 'bcove';
     else:
